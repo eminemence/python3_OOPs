a420cdf6c6b1560a28d3e7d8a9f59d3c47ba1e85
Animesh Bhadra <animeshb@archerimagine.com>
2020-05-04 09:56:12 +0530

    📦 NEW: methods in a python class

--- SRC/Chapter_02-Objects-In-Python/point.py
+++ SRC/Chapter_02-Objects-In-Python/point.py
@@ -1,5 +1,8 @@
 class Point:
-    pass
+    # self is a manditory name.
+    def reset(self):
+        self.x = 0
+        self.y = 0
 
 
 p1 = Point()
@@ -9,9 +12,18 @@ p2 = Point()
 p1.x = 5
 p1.y = 4
 
+p2.reset()
+print(p2.x, " ", p2.y)
 # Adding x,y attribute to Point P1
 p2.x = 3
 p2.y = 6
 
 print(p1.x, " ", p1.y)
+print(p2.x, " ", p2.y)
+
+p1.reset()
+print(p1.x, " ", p1.y)
+
+# Showing the use of self
+Point.reset(p2)
 print(p2.x, " ", p2.y)

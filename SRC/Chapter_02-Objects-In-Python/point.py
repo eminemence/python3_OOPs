62210dcfb6ac8f5ec122fb00ac93bbc488923988
Animesh Bhadra <animeshb@archerimagine.com>
2020-05-04 10:11:16 +0530

    📦 NEW:  methods move, reset and calculate_distance, with asset.

--- SRC/Chapter_02-Objects-In-Python/point.py
+++ SRC/Chapter_02-Objects-In-Python/point.py
@@ -1,8 +1,19 @@
+import math
+
+
 class Point:
+    # Move method
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
     # self is a manditory name.
     def reset(self):
-        self.x = 0
-        self.y = 0
+        self.move(0, 0)
+
+    # Calculate distance
+    def calculate_distance(self, other):
+        return math.sqrt(((self.x - other.x) ** 2) + ((self.y - other.y) ** 2))
 
 
 p1 = Point()
@@ -27,3 +38,17 @@ print(p1.x, " ", p1.y)
 # Showing the use of self
 Point.reset(p2)
 print(p2.x, " ", p2.y)
+
+# Calculate distance
+point1 = Point()
+point2 = Point()
+
+point1.reset()
+point2.move(5, 0)
+print(point2.calculate_distance(p1))
+
+#  The program asserts when the below line is false, and exits with AssertionError
+assert point2.calculate_distance(point1) == point1.calculate_distance(point2)
+
+point1.move(3, 4)
+print(point2.calculate_distance(p1))

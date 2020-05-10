83c69fcec90f0a61b7f83a761995a2817e2f647c
Animesh Bhadra <animeshb@archerimagine.com>
2020-05-10 20:47:05 +0530

    📦 NEW: new property case study ended.

--- SRC/Chapter_03-When-Object-Are-Alike/case_study/property.py
+++ SRC/Chapter_03-When-Object-Are-Alike/case_study/property.py
@@ -13,7 +13,7 @@ class Property(object):
         print("bathrooms: {}".format(self.num_baths))
 
     @staticmethod
-    def prompt_print():
+    def prompt_init():
         return dict(
             square_feet=input("Enter the square feet: "),
             beds=input("Enter number of bedrooms"),
@@ -38,21 +38,165 @@ class Apartment(Property):
 
     @staticmethod
     def prompt_init():
-        parent_init = Property.prompt_print()
-        laundary = ""
-        while laundary.lower() not in Apartment.valid_laundries:
-            laundary = input(
-                "What laundary facilitie does the property have? ({})".format(
-                    ", ".join(Apartment.valid_laundries)
-                )
-            )
+        parent_init = Property.prompt_init()
+        laundary = get_valid_input(
+            "What laundary facilities does the property have? ",
+            Apartment.valid_laundries,
+        )
 
-        balcony = ""
-        while balcony.lower() not in Apartment.valid_balconies:
-            balcony = input(
-                "Does the property have a balcony? ({})".format(
-                    ", ".join(Apartment.valid_balconies)
-                )
-            )
+        balcony = get_valid_input(
+            "Does the property have a balcony? ", Apartment.valid_balconies
+        )
         parent_init.update({"laundary": laundary, "balcony": balcony})
         return parent_init
+
+
+def get_valid_input(input_string, valid_options):
+    input_string += "({})".format(", ".join(valid_options))
+    response = input(input_string)
+    while response.lower() not in valid_options:
+        response = input(input_string)
+    return response
+
+
+class House(Property):
+    valid_garage = ("attached", "detached", "none")
+    valid_fenced = ("yes", "no")
+
+    def __init__(self, num_stories="", garage="", fenced="", **kwargs):
+        super().__init__(**kwargs)
+        self.garage = garage
+        self.fenced = fenced
+        self.num_stories = num_stories
+
+    def display(self):
+        super().display()
+        print("HOUSE DETAILS")
+        print("# of stories: {}".format(self.num_stories))
+        print("garage: {}".format(self.garage))
+        print("fenced yard: {}".format(self.fenced))
+
+    @staticmethod
+    def prompt_init():
+        parent_init = Property.prompt_init()
+        fenced = get_valid_input("Is the yard fenced? ", House.valid_fenced)
+        garage = get_valid_input("Is there a garage? ", House.valid_garage)
+        num_stories = input("How many Stories?")
+
+        parent_init.update(
+            {"fenced": fenced, "garage": garage, "num_stories": num_stories}
+        )
+        return parent_init
+
+
+class Purchase(object):
+    def __init__(self, price="", taxes="", **kwargs):
+        super().__init__(**kwargs)
+        self.price = price
+        self.taxes = taxes
+
+    def display(self):
+        super().display()
+        print("PURCHASE DETAILS")
+        print("selling price: {}".format(self.price))
+        print("estimated taxes: {}".format(self.taxes))
+
+    @staticmethod
+    def prompt_init():
+        return dict(
+            price=input("What is the selling price? "),
+            taxes=input("What are the estimated taxes?"),
+        )
+
+
+class Rental(object):
+    def __init__(self, furnished="", utilities="", rent="", **kwargs):
+        super().__init__(**kwargs)
+        self.furnished = furnished
+        self.rent = rent
+        self.utilities = utilities
+
+    def display(self):
+        super().display()
+        print("RENTAL DETAILS")
+        print("rent: {}".format(self.rent))
+        print("estimated utilities: {}".format(self.utilities))
+        print("furnished: {}".format(self.furnished))
+
+    @staticmethod
+    def prompt_init():
+        return dict(
+            rent=input("What is the monthly rent? "),
+            utilities=input("What the estimated utilities? "),
+            furnished=get_valid_input("Is the Property furnished? ", ("yes", "no")),
+        )
+
+
+class HouseRental(Rental, House):
+    @staticmethod
+    def prompt_init():
+        init = House.prompt_init()
+        init.update(Rental.prompt_init())
+        return init
+
+
+class ApartmentRental(Rental, Apartment):
+    @staticmethod
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Rental.prompt_init())
+        return init
+
+
+class ApartmentPurchase(Purchase, Apartment):
+    @staticmethod
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Purchase.prompt_init())
+        return init
+
+
+class HousePurchase(object):
+    @staticmethod
+    def prompt_init():
+        init = House.prompt_init()
+        init.update(Purchase.prompt_init())
+        return init
+
+
+class Agent(object):
+    type_map = {
+        ("house", "rental"): HouseRental,
+        ("house", "purchase"): HousePurchase,
+        ("apartment", "rental"): ApartmentRental,
+        ("apartment", "purchase"): ApartmentPurchase,
+    }
+
+    def __init__(self):
+        self.property_list = []
+
+    def display_properties(self):
+        for property in self.property_list:
+            property.display()
+
+    def add_property(self):
+        property_type = get_valid_input(
+            "What type of property? ", ("house", "apartment")
+        ).lower()
+        payment_type = get_valid_input(
+            "What payment type? ", ("purchase", "rental")
+        ).lower()
+
+        PropertyClass = self.type_map[(property_type, payment_type)]
+        init_args = PropertyClass.prompt_init()
+        self.property_list.append(PropertyClass(**init_args))
+
+
+if __name__ == "__main__":
+    # init = HouseRental.prompt_init()
+    # house = HouseRental(**init)
+    # house.display()
+    agent = Agent()
+    agent.add_property()
+    agent.add_property()
+    agent.display_properties()

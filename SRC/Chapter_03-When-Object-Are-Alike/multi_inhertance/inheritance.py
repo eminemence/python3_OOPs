b6edad9fd6a3e7e25d37fe4164793d76dcb79dd8
Animesh Bhadra <animeshb@archerimagine.com>
2020-05-10 16:34:06 +0530

    👌 IMPROVE: Calling Super helps in modify the call stack.

--- SRC/Chapter_03-When-Object-Are-Alike/multi_inhertance/inheritance.py
+++ SRC/Chapter_03-When-Object-Are-Alike/multi_inhertance/inheritance.py
@@ -10,7 +10,8 @@ class LeftSubClass(BaseClass):
     num_left_calls = 0
 
     def call_me(self):
-        BaseClass.call_me(self)
+        # BaseClass.call_me(self)
+        super().call_me()
         print("Calling method on Left Sub Class")
         self.num_left_calls += 1
 
@@ -19,7 +20,8 @@ class RightSubClass(BaseClass):
     num_right_calls = 0
 
     def call_me(self):
-        BaseClass.call_me(self)
+        # BaseClass.call_me(self)
+        super().call_me()
         print("Calling method on right sub class")
         self.num_right_calls += 1
 
@@ -28,13 +30,15 @@ class SubClass(LeftSubClass, RightSubClass):
     num_sub_calls = 0
 
     def call_me(self):
-        LeftSubClass.call_me(self)
-        RightSubClass.call_me(self)
+        # LeftSubClass.call_me(self)
+        # RightSubClass.call_me(self)
+        super().call_me()
         print("Calling method on SubClass")
         self.num_sub_calls += 1
 
 
 if __name__ == "__main__":
     s = SubClass()
+    print(SubClass.__mro__)
     s.call_me()
     print(s.num_sub_calls, s.num_right_calls, s.num_left_calls, s.num_base_calls)

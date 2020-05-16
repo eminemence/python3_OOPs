9284af741a916a981f140e5d876cc6fc7281331a
Animesh Bhadra <animeshb@archerimagine.com>
2020-05-16 11:23:16 +0530

    📦 NEW: Handling 2 exception together

---
+++ SRC/Chapter_04-Expecting-The-Unexpected/04_funny_division.py
@@ -0,0 +1,15 @@
+    def funny_division(anumber):
+        try:
+            if anumber == 13:
+                raise ValueError("13 is an unlucky number")
+            return 100 / anumber
+        except (ZeroDivisionError, TypeError):
+            return "Enter a number other than zero"
+
+
+if __name__ == "__main__":
+    # The below line produces a ZeroDivisionError, which is caught
+    # print(funny_division(0))
+    print(funny_division(50.0))
+    # the below raises a TypeErro
+    print(funny_division("hello"))

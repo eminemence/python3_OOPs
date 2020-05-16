57edd81e0236bb40f4ac3fd7b0f3d1ba338c048d
Animesh Bhadra <animeshb@archerimagine.com>
2020-05-16 10:36:30 +0530

    📦 NEW: raising exception

---
+++ SRC/Chapter_04-Expecting-The-Unexpected/02_non_return.py
@@ -0,0 +1,17 @@
+def no_return():
+    print("I am about to raise an exception")
+    raise Exception("This is always raised")
+    print("This line will never execute")
+    return "I won't be returned."
+
+
+def call_exceptor():
+    print("Call_exceptor starts here")
+    no_return()
+    print("an exception was raised")
+    print("...so these lines don't run")
+
+
+if __name__ == "__main__":
+    # no_return()
+    call_exceptor()

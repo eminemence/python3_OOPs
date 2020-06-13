9e5c2ed52510532bff88dca0dd4f2649b8e4beb2
Animesh Bhadra <animeshb@archerimagine.com>
2020-06-13 17:36:42 +0530

    👌 IMPROVE: introduced the cursor class

--- SRC/Chapter_05-When-To-Use-Object-Oriented-Programming/case_study/document.py
+++ SRC/Chapter_05-When-To-Use-Object-Oriented-Programming/case_study/document.py
@@ -1,26 +1,49 @@
 class Document(object):
     def __init__(self):
         self.characters = []
-        self.cursor = 0
+        self.cursor = Cursor(self)
         self.filename = ""
 
     def insert(self, character):
-        self.characters.insert(self.cursor, character)
-        self.cursor += 1
+        self.characters.insert(self.cursor.position, character)
+        self.cursor.forward()
 
     def delete(self):
-        del self.characters[self.cursor]
+        del self.characters[self.cursor.position]
 
     def save(self):
         f = open(self.filename, "w")
         f.write("".join(self.characters))
         f.close()
 
+    @property
+    def string(self):
+        return "".join(self.characters)
+
+
+class Cursor(object):
+    def __init__(self, document):
+        self.document = document
+        self.position = 0
+
     def forward(self):
-        self.cursor += 1
+        self.position += 1
 
     def back(self):
-        self.cursor -= 1
+        self.position -= 1
+
+    def home(self):
+        while self.document.characters[self.position - 1] != "\n":
+            self.position -= 1
+            if self.position == 0:
+                break
+
+    def end(self):
+        while (
+            self.position < len(self.document.characters)
+            and self.document.characters[self.position] != "\n"
+        ):
+            self.position += 1
 
 
 if __name__ == "__main__":
@@ -31,9 +54,14 @@ if __name__ == "__main__":
     doc.insert("l")
     doc.insert("l")
     doc.insert("o")
+    doc.insert("\n")
+    doc.insert("w")
+    doc.insert("o")
+    doc.insert("r")
+    doc.insert("l")
+    doc.insert("d")
     print("".join(doc.characters))
-
-    doc.back()
-    doc.delete()
-    doc.insert("p")
+    print(doc.string)
+    doc.cursor.home()
+    doc.insert("*")
     print("".join(doc.characters))
